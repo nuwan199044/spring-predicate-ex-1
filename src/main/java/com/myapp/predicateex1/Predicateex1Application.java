36b6bed2657c11cc0da9bb35319a4596990464df
Nuwan Weerasinghe <nuwanw@zone24x7.lk>
2024-07-18 15:15:18 +0530

    initial code

---
+++ src/main/java/com/myapp/predicateex1/Predicateex1Application.java
@@ -0,0 +1,13 @@
+package com.myapp.predicateex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Predicateex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Predicateex1Application.class, args);
+	}
+
+}
